b5379bfef81780ebe4b99ef2148fe58b899bc0c5
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-06 22:48:17 +0530

    feat: Profile Route Update user Route

--- Backend/routes/profileRoute.js
+++ Backend/routes/profileRoute.js
@@ -2,4 +2,5 @@ import express from "express";
 import { getUserDetails, updateUser } from "../controller/profileController.js";
 const router = express.Router();
 router.get("/get/:id", getUserDetails);
+router.post("/update/:id", updateUser);
 export default router;
