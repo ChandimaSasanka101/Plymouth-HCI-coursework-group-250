7d74941db2dc88cff94529fba84edbd44ebf5d07
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-06 22:40:25 +0530

    feat: Auth Path Added

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ import LoginRoute from "./routes/loginRoute.js";
 import RegisterRoute from "./routes/registerRoute.js";
 import DesignRoute from "./routes/designRoute.js";
 import ProfileRoute from "./routes/profileRoute.js";
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 connectDB();
 const app = express();
@@ -25,5 +26,7 @@ app.use("/api/login", LoginRoute);
 app.use("/api/register", RegisterRoute);
 app.use("/api/design", DesignRoute);
 app.use("/api/profile", ProfileRoute);
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
