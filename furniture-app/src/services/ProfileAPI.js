dd63b2869df9830d7920516cd228950ec4927171
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-06 22:49:08 +0530

    feat: Profile API get user details API

--- furniture-app/src/services/ProfileAPI.js
+++ furniture-app/src/services/ProfileAPI.js
@@ -2,4 +2,20 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
+
+export const UserDetailAPI = {
+  getUserDetails: async (Id) => {
+    try {
+      const response = await API.get(`/profile/get/${Id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Save Error:", error);
+      return {
+        success: false,
+        message:
+          error.response?.data?.message ||
+          "Network Error: Is the server running?",
+      };
+    }
+  },
 export default UserDetailAPI;
