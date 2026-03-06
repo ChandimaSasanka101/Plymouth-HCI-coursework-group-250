d4171d14679a63d09e31054f05b9b800fe54040c
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-06 22:39:36 +0530

    feat: Login Path Added

--- Backend/server.js
+++ Backend/server.js
@@ -3,5 +3,20 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
+import LoginRoute from "./routes/loginRoute.js";
+dotenv.config();
+connectDB();
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+//Routes
+app.use("/api/login", LoginRoute);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
