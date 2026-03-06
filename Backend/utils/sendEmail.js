42b00272d1acaffef182b670a75922fd5845519f
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-06 22:42:07 +0530

    feat: Send Email function created

---
+++ Backend/utils/sendEmail.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  //Create a Transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  //Define the email options
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+  };
+
+  //Send the email
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+};
+
+export default sendEmail;
