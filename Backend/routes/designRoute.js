83db61822ed1a37f22fb045fe3239c7cab902672
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-07 11:31:41 +0530

    feat: Design route created

---
+++ Backend/routes/designRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {
+  createDesign,
+  getDesign,
+  deleteDesign,
+  seletedDesign,
+  updateDesign,
+} from "../controller/designController.js";
+export default router;
