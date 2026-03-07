5ce4d6405cb6d303abc48f75f16cbc21e178037e
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-07 11:30:29 +0530

    feat: AuthAPI passwordpassword logic

--- furniture-app/src/services/authAPI.js
+++ furniture-app/src/services/authAPI.js
@@ -3,4 +3,20 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+export const PasswordRest = {
+  forgotPassword: async (email) => {
+    try {
+      const response = await API.post("/auth/forgotpassword", { email });
+      return response.data;
+    } catch (error) {
+      console.error("Save Error:", error);
+      return {
+        success: false,
+        message:
+          error.response?.data?.message ||
+          "Network Error: Is the server running?",
+      };
+    }
+  },
+};
 export default PasswordRest;
