0b81bce1fc7e954c4d57f4cba740564a4adb8568
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-07 11:29:28 +0530

    feat: AuthRoute forgotpassword route added

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,4 +2,8 @@ import express from "express";
 import { forgotPassword, resetPassword } from "../controller/authController.js";
 
 const router = express.Router();
+
+// Route for requesting the email
+router.post("/forgotpassword", forgotPassword);
+
 export default router;
