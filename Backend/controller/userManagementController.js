caff24d2cbc53f12f693e1be272c937ebb520591
Duhudu <10953004@students.plymouth.ac.uk>
2026-03-07 11:33:54 +0530

    feat: User management controller ban a user backend logic

--- Backend/controller/userManagementController.js
+++ Backend/controller/userManagementController.js
@@ -20,3 +20,33 @@ export const getAllUsers = async (req, res) => {
     });
   }
 };
+//Functiont to ban a user
+export const BanUser = async (req, res) => {
+  console.log("User  Controller Ban function hit");
+  const userId = req.params.id || req.params.Id;
+  try {
+    const user = await User.findByIdAndUpdate(userId, { status: "Banned" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ success: true, message: "Account banned successful!" });
+  } catch (err) {
+    console.error("ERROR in Profile Update user function:", err);
+    res.status(500).send(err.message);
+  }
+};
+//Function to Unban a user
+export const UnBanUser = async (req, res) => {
+  console.log("User  Controller UnBan function hit");
+  const userId = req.params.id || req.params.Id;
+  try {
+    const user = await User.findByIdAndUpdate(userId, { status: "Active" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ success: true, message: "Account unbanned successful!" });
+  } catch (err) {
+    console.error("ERROR in Profile Update user function:", err);
+    res.status(500).send(err.message);
+  }
+};
